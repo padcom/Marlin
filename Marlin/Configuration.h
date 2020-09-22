0dcc54405880102feb0484bb5ace462c8e757a23
Matthias Hryniszak <mhryn@softserveinc.com>
2020-09-22 20:54:35 +0200

    Updated steps per mm with the microstepping set to 256

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -753,7 +753,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 1600, 147 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 1280, 1280, 1600, 147 }
 
 /**
  * Default Max Feed Rate (mm/s)
